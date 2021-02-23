c8d858863eb122deaf893a22a7f6a15741cba991
Sohaib Mubashir <sohaibmh@yahoo.com>
2021-02-23 14:56:04 +0000

    first commit

---
+++ pages/washing-machine-repair.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function WashingMachineRepair() {
+  return (
+    <>
+      <h1>First Post</h1>
+
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+
+      <Image
+        src="/images/oven-repair-model.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </>
+  );
+}
