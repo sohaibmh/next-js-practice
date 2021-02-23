d0877c8c4eb3be75a2319de3e32e108281faac05
Sohaib Mubashir <sohaibmh@yahoo.com>
2021-02-23 16:31:27 +0000

    added css modules

--- pages/washing-machine-repair.js
+++ pages/washing-machine-repair.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
+import Layout from "../components/layout";
 
 export default function WashingMachineRepair() {
   return (
-    <>
-      <h1>First Post</h1>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
 
+      <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
@@ -18,6 +23,6 @@ export default function WashingMachineRepair() {
         width={144} // Desired size with correct aspect ratio
         alt="Your Name"
       />
-    </>
+    </Layout>
   );
 }
